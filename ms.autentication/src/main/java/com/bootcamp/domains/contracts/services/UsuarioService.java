2f29bafe19bf486060882ad2c9a924166249e3f4
miguelZM23 <miguel.zapata-martinez@capgemini.com>
2022-04-07 13:50:08 +0200

    renombrado paquetes

---
+++ ms.autentication/src/main/java/com/bootcamp/domains/contracts/services/UsuarioService.java
@@ -0,0 +1,8 @@
+package com.bootcamp.domains.contracts.services;
+
+import com.bootcamp.domains.entities.Usuario;
+
+public interface UsuarioService extends ProjectionDomainService<Usuario, String > {
+
+}
+
--- ms.autentication/src/main/java/com/capgemini/domains/contracts/services/UsuarioService.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.domains.contracts.services;
-
-import com.capgemini.domains.entities.Usuario;
-
-public interface UsuarioService extends ProjectionDomainService<Usuario, String > {
-
-}
-
